2c7a9ee8f75551973e8102ae96d13faa0459404a
Rudiko777 <chakryan_rudik@mail.ru>
2024-03-13 01:41:21 +0300

    firstCommit

---
+++ src/components/CardDirectory/CardProducts.props.ts
@@ -0,0 +1,3 @@
+export interface CardProductsProps{
+    name: string
+}
---
+++ src/components/CardDirectory/CardProducts.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './CardProducts.module.css'
+
+const CardProducts = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default CardProducts;
